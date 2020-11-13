989f34a569501e6d5e88836769b7ea63dfa9cab3
ruizaj13 <ruizaj13@gmail.com>
2020-11-13 10:21:41 -0800

    schema created and applied, form validation functional

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,48 +1,76 @@
-import React from "react";
-import {Route, Link} from 'react-router-dom';
-import '../App.css'
+import React, {useState, useEffect} from "react";
+import * as yup from 'yup';
+import axios from 'axios';
+import Schema from './Schema'
+import '../App.css';
+
+const initialFormState = {
+    name: '',
+    size: '',
+    pepperoni: false,
+    ham: false,
+    pineapple: false,
+    bacon: false,
+    instructions: '',
+
+}
 
 const PizzaForm = () => {
+
+    const [formData,setFormData] = useState(initialFormState)
+    const [disabled, setDisabled] = useState(true)
+
+    const onChange = (event) => {
+        const {checked, value, type, name} = event.target
+        const valueToUse = type === 'checkbox' ? checked : value
+        setFormData({...formData, [name]: valueToUse})
+    }
+
+    useEffect(() => {
+        Schema.isValid(formData).then(valid => setDisabled(!valid))
+    }, [formData])
+
+
 
     return (
         <div>
             <form>
                 <div>
                     <label>
-                        Name: <input type='text' name='name'  />
+                        Name: <input onChange={onChange} value={formData.name} type='text' name='name'  />
                     </label>
                 </div>
                 <div>
                     <label>
-                        Size: <select name='size'>
-                                <option>--Select A Size--</option>
-                                <option>Small</option>
-                                <option>Medium</option>
-                                <option>Large</option>
+                        Size: <select onChange={onChange} value={formData.size} name='size'>
+                                <option value=''>--Select A Size--</option>
+                                <option value='1'>Small</option>
+                                <option value='2'>Medium</option>
+                                <option value='3'>Large</option>
                               </select>
                     </label>
                 </div>
                     <h4>Toppings</h4>
                 <div className='toppings'>
                     <label>
-                        Peperroni:<input type='checkbox' name='pepperoni'/>
+                        Peperroni:<input onChange={onChange} checked={formData.pepperoni} type='checkbox' name='pepperoni'/>
                     </label>
                     <label>
-                        Ham:<input type='checkbox' name='ham'/>
+                        Ham:<input onChange={onChange} checked={formData.ham} type='checkbox' name='ham'/>
                     </label>
                     <label>
-                        Pineapple:<input type='checkbox' name='pineapple'/>
+                        Pineapple:<input onChange={onChange} checked={formData.pineapple} type='checkbox' name='pineapple'/>
                     </label>
                     <label>
-                        Bacon:<input type='checkbox' name='bacon'/>
+                        Bacon:<input onChange={onChange} checked={formData.bacon} type='checkbox' name='bacon'/>
                     </label>
                 </div>
                 <div>
                     <label>
-                        Special Instructions: <input type='text' name='instructions'/>
+                        Special Instructions: <input onChange={onChange} value={formData.instructions} type='text' name='instructions'/>
                     </label>
                 </div>
-                <button id='submitBtn'>Submit Order!</button>
+                <button id='submitBtn' disabled={disabled}>Submit Order!</button>
             </form>
         </div>
     )
---
+++ src/components/Schema.js
@@ -0,0 +1,14 @@
+import * as yup from 'yup'
+
+
+const Schema = yup.object().shape({
+    name: yup.string().required('user is required').min(2, 'name needs to be atleast 2 characters'),
+    size: yup.string().oneOf(['1', '2', '3'], 'Size Matters'),
+    pepperoni: yup.boolean(),
+    ham: yup.boolean(),
+    pineapple: yup.boolean(),
+    bacon: yup.boolean(),
+    instructions: yup.string()
+})
+
+export default Schema;
